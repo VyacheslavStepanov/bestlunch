dd74cf4f5bc825c124759e5e58173f69525b3f46
Vyacheslav Stepanov <sweb1713@gmail.com>
2018-05-29 19:20:32 +0300

    added add product form

--- src/main/java/com/sweb/bestlunch/controllers/lunch/ProductController.java
+++ src/main/java/com/sweb/bestlunch/controllers/lunch/ProductController.java
@@ -1,11 +1,15 @@
 package com.sweb.bestlunch.controllers.lunch;
 
+import com.sweb.bestlunch.dtos.CustomerDTO;
 import com.sweb.bestlunch.entities.customer.Customer;
 import com.sweb.bestlunch.entities.lunch.Product;
 import com.sweb.bestlunch.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,5 +35,29 @@ public class ProductController {
         model.put("products", products);
         return new ModelAndView("products", model);
     }
+
+    @PostMapping("/")
+    public String addOrUpdate(Model model, @ModelAttribute("product") Product product){
+
+        if (product.getName() != null && product.getName().length()>0
+                && product.getDescription()!= null && product.getDescription().length()>0
+                && product.getProductCategory() != null) {
+
+            if (service.save(product)) {
+                return "redirect:/products";
+            } else {
+                model.addAttribute("errorMessage","can't save product");
+                return "productForm";
+            }
+        }
+        model.addAttribute("errorMessage","fill all inputs");
+        return "productForm";
+    }
+    @GetMapping("/add")
+    public String showAddProductPage(Model model){
+        model.addAttribute("product",new Product());
+        return "productForm";
+    }
+
 
 }
